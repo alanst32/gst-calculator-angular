f8f7e3c54c9d504df36c70881b24663b17d64533
alanst32 <alanst32@gmail.com>
2018-08-26 09:45:21 +1000

    Fixing test cases

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroupDirective, FormGroup, NgForm, Validators } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule} from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing'
+import {ErrorStateMatcher } from '@angular/material/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let formGroup: FormGroup;
+  let usernameControl: FormControl;
+  let passwordControl: FormControl;
+ 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule, 
+        HttpClientModule, 
+        MatInputModule, 
+        ReactiveFormsModule, 
+        RouterTestingModule
+      ],
+      providers: [
+        FormBuilder
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +36,32 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+
+    // // // Mock the form controls
+    // // usernameControl = new FormControl('', Validators.compose([
+    // //   Validators.required,
+    // //   Validators.email,
+    // // ]));
+
+    // // passwordControl = new FormControl('', Validators.required);
+
+    // // formGroup = new FormGroup( {
+    // //   usernameControl, passwordControl
+    // // });
+
+    // // component.validationsForm = formGroup;
+
+    // // errorStateMatcher = new ErrorStateMatcher();
+
+    // component.matcher = errorStateMatcher;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+ 
 });
--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,4 +1,15 @@
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroupDirective, FormGroup, NgForm, Validators } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule} from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing'
+import { APP_BASE_HREF } from '@angular/common';
+
+
+import { MatPaginatorModule, MatTableModule } from '@angular/material';
 
 import { MainComponent } from './main.component';
 
@@ -8,7 +19,21 @@ describe('MainComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ MainComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule, 
+        HttpClientModule, 
+        MatInputModule, 
+        MatPaginatorModule,
+        MatTableModule,
+        ReactiveFormsModule, 
+        RouterTestingModule
+      ],
+      providers: [
+        FormBuilder,
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ] 
     })
     .compileComponents();
   }));
