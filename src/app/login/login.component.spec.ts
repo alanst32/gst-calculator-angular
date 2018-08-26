6ca228b981c74480c8fbdda0c592cf843bea932d
alanst32 <alanst32@gmail.com>
2018-08-26 11:10:55 +1000

    more Test Cases

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,19 +1,23 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
-import { FormControl, FormGroupDirective, FormGroup, NgForm, Validators } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule} from '@angular/material/input';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing'
-import {ErrorStateMatcher } from '@angular/material/core';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+
+interface User {
+  username: String,
+  password: String
+}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let formGroup: FormGroup;
-  let usernameControl: FormControl;
-  let passwordControl: FormControl;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,6 +26,7 @@ describe('LoginComponent', () => {
         BrowserAnimationsModule,
         FormsModule, 
         HttpClientModule, 
+        HttpClientTestingModule,
         MatInputModule, 
         ReactiveFormsModule, 
         RouterTestingModule
@@ -37,23 +42,8 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
 
-    // // // Mock the form controls
-    // // usernameControl = new FormControl('', Validators.compose([
-    // //   Validators.required,
-    // //   Validators.email,
-    // // ]));
-
-    // // passwordControl = new FormControl('', Validators.required);
-
-    // // formGroup = new FormGroup( {
-    // //   usernameControl, passwordControl
-    // // });
-
-    // // component.validationsForm = formGroup;
-
-    // // errorStateMatcher = new ErrorStateMatcher();
-
-    // component.matcher = errorStateMatcher;
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
 
     fixture.detectChanges();
   });
@@ -62,6 +52,92 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should invalidate required forms', fakeAsync(() =>{
+    component.validationsForm.controls['usernameControl'].setValue('');
+    component.validationsForm.controls['passwordControl'].setValue('');
+   
+    expect(component.validationsForm.controls['usernameControl'].hasError('required')).toBe(true);
+    expect(component.validationsForm.controls['passwordControl'].hasError('required')).toBe(true);
 
- 
+    expect(component.validationsForm.invalid).toBe(true);
+  }));
+
+  it('should invalidate email format forms', fakeAsync(() =>{
+    const tesUser = {
+      email: 'test.com'
+    }
+
+    component.validationsForm.controls['usernameControl'].setValue(tesUser.email);
+   
+    expect(component.validationsForm.controls['usernameControl'].hasError('email')).toBe(true);
+    expect(component.validationsForm.controls['usernameControl'].hasError('required')).toBe(false);
+
+    expect(component.validationsForm.invalid).toBe(true);
+  }));
+
+  it('should validate input forms', fakeAsync(() =>{
+    const tesUser = {
+      email: 'test@test.com',
+      password: '12345'
+    }
+
+    component.validationsForm.controls['usernameControl'].setValue(tesUser.email);
+    component.validationsForm.controls['passwordControl'].setValue(tesUser.email);
+
+    expect(component.validationsForm.controls['usernameControl'].hasError('email')).toBe(false);
+    expect(component.validationsForm.controls['usernameControl'].hasError('required')).toBe(false);
+
+    expect(component.validationsForm.controls['passwordControl'].hasError('required')).toBe(false);
+
+    expect(component.validationsForm.invalid).toBe(false);
+  }));
+
+
+  it('should call login_btnClick', () => {
+
+    fixture.detectChanges();
+    spyOn(component, 'login_btnClick');
+  
+    let btn = fixture.debugElement.query(By.css('button'));
+
+    btn.triggerEventHandler('click', null);
+  
+    fixture.detectChanges();
+
+    expect(component.login_btnClick).toHaveBeenCalled();
+  })
+
+  it('should validate the user password', () => {
+
+    const testUrl = "/api/findUser/test@test.com";
+
+    const responseUser: User = {
+      username: 'test@test.com',
+      password: '12345'
+    };
+
+    // Test HTTP Request
+    httpClient.get<User>(testUrl)
+      .subscribe(data => 
+
+         // When observable resolves, result should match test data
+        expect(data).toEqual(responseUser)
+      );
+
+
+    // The following `expectOne()` will match the request's URL.
+    // If no requests or multiple requests matched that URL
+    // `expectOne()` would throw.
+    const req = httpTestingController.expectOne(testUrl);
+    
+    // Respond with mock data, causing Observable to resolve.
+    // Subscribe callback asserts that correct data was returned.
+    req.flush(responseUser);
+
+    // Finally, assert that there are no outstanding requests.
+    httpTestingController.verify();
+
+
+  });
+
 });
--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { FormControl, FormGroupDirective, FormGroup, NgForm, Validators } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule} from '@angular/material/input';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing'
 import { APP_BASE_HREF } from '@angular/common';
 
@@ -12,10 +12,21 @@ import { APP_BASE_HREF } from '@angular/common';
 import { MatPaginatorModule, MatTableModule } from '@angular/material';
 
 import { MainComponent } from './main.component';
+import { By } from '@angular/platform-browser';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+
+interface CalcHistory {
+  inputPrice: Number,
+  priceAfter: Number,
+  gstAmount: Number
+}
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,6 +35,7 @@ describe('MainComponent', () => {
         BrowserAnimationsModule,
         FormsModule, 
         HttpClientModule, 
+        HttpClientTestingModule,
         MatInputModule, 
         MatPaginatorModule,
         MatTableModule,
@@ -47,4 +59,36 @@ describe('MainComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should invalidate Price forms', () =>{
+    component.mainFormGroup.controls['priceControl'].setValue('');
+   
+    expect(component.mainFormGroup.controls['priceControl'].hasError('required')).toBe(true);
+
+    expect(component.mainFormGroup.invalid).toBe(true);
+  });
+
+  it('should validate Price has been informed ', () =>{
+    component.mainFormGroup.controls['priceControl'].setValue(1234);
+   
+    expect(component.mainFormGroup.controls['priceControl'].hasError('required')).toBe(false);
+
+    expect(component.mainFormGroup.invalid).toBe(false);
+  });
+
+  it('should call add_btnClick', () => {
+
+    fixture.detectChanges();
+    spyOn(component, 'add_btnClick');
+  
+    let btn = fixture.debugElement.query(By.css('button'));
+
+    btn.triggerEventHandler('click', null);
+    
+    fixture.detectChanges();
+
+    expect(component.add_btnClick).toHaveBeenCalled();
+  });
+
+ 
 });
